d235b4475134dc88b85469bac61b76fcdfe6da6c
Jina Choi <dev.jina0120@gmail.com>
2022-06-07 23:13:18 +0900

    [백준/JAVA] (CLASS1) 10172번 개

--- src/solved/ac/class1/q10171.java
+++ src/solved/ac/class1/q10171.java
@@ -4,7 +4,7 @@ public class q10171 {
     public static void main(String[] args) {
         // 두번째 풀이
         System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
-        
+
         // 첫번째 풀이
 //        System.out.println("\\    /\\");
 //        System.out.println(" )  ( \')");
