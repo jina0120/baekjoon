ca85d5d2b618f8033baea8ff1fc9a321b207dbc2
Jina Choi <dev.jina0120@gmail.com>
2022-06-08 21:40:50 +0900

    [백준/JAVA] (CLASS1) 11720번 숫자의 합

---
+++ src/solved/ac/class1/q11720.java
@@ -0,0 +1,18 @@
+package solved.ac.class1;
+
+import java.io.*;
+
+public class q11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+        System.out.print(sum);
+    }
+}
