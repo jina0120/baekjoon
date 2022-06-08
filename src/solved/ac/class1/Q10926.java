35949629bd5337e45600b277ecce3919a89ffc4b
Jina Choi <dev.jina0120@gmail.com>
2022-06-08 23:04:29 +0900

    [백준/JAVA] 10926번 ??!

---
+++ src/solved/ac/class1/Q10926.java
@@ -0,0 +1,20 @@
+package solved.ac.class1;
+
+import java.io.*;
+import java.util.*;
+
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+//        // 두번째 풀이
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        System.out.print(br.readLine() + "??!");
+
+        // 첫번째 풀이
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringBuilder st = new StringBuilder();
+        System.out.print(st.append(s).append("??!"));
+    }
+}
