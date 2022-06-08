39f394a00cdff406c16dbd4d7cf73281b342cc72
Jina Choi <dev.jina0120@gmail.com>
2022-06-08 17:07:50 +0900

    [백준/JAVA] (CLASS1) 10950번 A+B - 3

---
+++ src/solved/ac/class1/q10950.java
@@ -0,0 +1,26 @@
+package solved.ac.class1;
+
+import java.io.*;
+import java.util.*;
+
+public class q10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        // StringTokenizer 전역 변수 생성
+        StringTokenizer st;
+        // 결과를 출력할 스트링빌더 생성
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            // 입력받고 공백을 기준으로 쪼개서 int를 받아준다.
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            // 결과 빌더에 저장
+            sb.append(a + b).append("\n");
+        }
+        // 출력
+        System.out.print(sb);
+    }
+}
