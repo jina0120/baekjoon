238be2d15496d9f468b783c4a3c210a938f96ada
Jina Choi <dev.jina0120@gmail.com>
2022-06-09 23:44:46 +0900

    [백준/JAVA] 14681번 사분면 고르기

---
+++ src/solved/ac/class1/Q14681.java
@@ -0,0 +1,14 @@
+package solved.ac.class1;
+
+import java.io.*;
+
+public class Q14681 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.print(x > 0 ?(y>0 ? "1" : "4"):(y>0 ? "2" : "3"));
+    }
+}
