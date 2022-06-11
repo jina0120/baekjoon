95972d881e8dc1e68001e8d3404f2d32bc3c337d
Jina Choi <dev.jina0120@gmail.com>
2022-06-11 16:44:27 +0900

    [백준/JAVA] 11022번 A+B - 8

---
+++ src/solved/ac/class1/Q11022.java
@@ -0,0 +1,39 @@
+package solved.ac.class1;
+
+import java.io.*;
+import java.util.*;
+
+public class Q11022 {
+    public static void main(String[] args) throws IOException {
+        // 두번째 풀이
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i).append(": ").append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n");
+        }
+        System.out.print(sb);
+
+//        // 첫번째 풀이
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        StringBuilder sb = new StringBuilder();
+//
+//        int t = Integer.parseInt(br.readLine());
+//        for (int i = 1; i <= t; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//
+//            sb.append("Case #");
+//            sb.append(String.format("%d: %d + %d = %d\n", i, a, b, a + b));
+//        }
+//        System.out.print(sb);
+    }
+}
