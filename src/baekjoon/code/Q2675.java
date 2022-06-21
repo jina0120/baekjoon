8fb782b70a693eb4a685887cf51d1fd952aac20b
Jina Choi <dev.jina0120@gmail.com>
2022-06-22 00:09:07 +0900

    [백준/JAVA] 2675번 문자열 반복

---
+++ src/baekjoon/code/Q2675.java
@@ -0,0 +1,26 @@
+package baekjoon.code;
+
+import java.io.*;
+import java.util.*;
+
+public class Q2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int r = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < r; k++) {
+                    sb.append(s.charAt(j));
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
