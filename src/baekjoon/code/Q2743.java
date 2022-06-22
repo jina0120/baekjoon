62ae7d8d41d084f890d5c0fce940219275c399ff
Jina Choi <dev.jina0120@gmail.com>
2022-06-22 16:38:31 +0900

    [백준/JAVA] 2743번 단어 길이 재기

--- src/baekjoon/code/Q2743.java
+++ src/baekjoon/code/Q2743.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Q2743 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s = br.readLine();
         System.out.print(s.length());
     }
